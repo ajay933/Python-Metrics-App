b58485013a0f67c2f10694780652bff9106baf2a
ajay933 <ajaymorya538@gmail.com>
2024-05-17 13:05:31 +0530

    update my code content

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.INFO)
 @app.route('/')
 def hello():
     logger.info('Hello endpoint accessed')
-    return 'Hello, World!'
+    return 'Hello, World, Ajay code!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
